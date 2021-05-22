aa3d94f61df5be28865c10e176b167e8d6729a87
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-22 13:49:16 +0200

    Initial state with dependencies setted

---
+++ src/main/java/be/digitcom/course/management/CourseManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package be.digitcom.course.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementMicroserviceApplication.class, args);
+	}
+
+}
